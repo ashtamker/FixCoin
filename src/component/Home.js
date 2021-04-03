372b9989f136da293c7eea86ab0cb2f66e10281c
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-04 02:54:50 +0300

    starting list

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,22 +1,49 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import Coin from './Coin';
 
 const Home = () => {
     const [coinsList, setCoinsList] = useState([]);
+    const [searchCoin, setSearchCoin] = useState('');
 
     useEffect(() => {
     axios
     .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(results => {
         setCoinsList(results.data);
-        console.log(results.data);
 
-    });
-});
+    }).catch(
+        error => console.log(error));
+},[]);
+
+const onChangeSearch = e => {
+    setSearchCoin(e.target.value)
+}
+
+const filterTheCoins = coinsList.filter(coin => 
+   coin.name.toLowerCase().includes(searchCoin.toLocaleLowerCase()))
 
     return(
-        <div>
-            check hello?
+        <div className="coin-app">
+            <div>
+                <h1 className="coin-text">Search a coins</h1>
+                <form>
+                    <input type="text" placeholder="Bitcoin" className="coin-input" onChange={onChangeSearch}></input> 
+
+                </form>
+            </div>
+            {filterTheCoins.map(coin => {
+                return (
+                    <Coin 
+                    key={coin.id} 
+                    name={coin.name} 
+                    image={coin.image} 
+                    symbol={coin.symbol} 
+                    volume={coin.market_cap} 
+                    price={coin.current_price} 
+                    /> 
+                )
+            })}
         </div>
     )
 }
