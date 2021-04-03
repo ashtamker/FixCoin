33aa90139fbdfbb57528be15c52d0ea34439fcb1
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-04 02:12:31 +0300

    api check

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const Home = () => {
+    const [coinsList, setCoinsList] = useState([]);
 
-    
+    useEffect(() => {
+    axios
+    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(results => {
+        setCoinsList(results.data);
+        console.log(results.data);
+
+    });
+});
 
     return(
         <div>
