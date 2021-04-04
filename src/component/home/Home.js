c8cb33d455ab9db571a269a3a27418160b4fc683
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-04 14:22:50 +0300

    basic style

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import Coin from './coin/Coin';
+import Coin from '../coin/Coin';
+import './home.css';
 
 const Home = () => {
     const [coinsList, setCoinsList] = useState([]);
