fd1c18a09ca5aa606aef0df12a440891889b64ce
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-04 17:31:29 +0300

    /

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './component/home/Home';
-
+import Favorite from './component/userfav/Favorite';
 
 
 function App() {
   return (
     <div className="App">
-     <Home />
+      <BrowserRouter>
+      <Route exact path="/" component={Home}/>
+      </BrowserRouter>
+    
     </div>
   );
 }
