1e1ce793da6877c54e80dcd8198d6141754204ca
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-07 01:48:38 +0300

    default fav list

--- src/component/userpage/UserPage.js
+++ src/component/userpage/UserPage.js
@@ -1,31 +1,56 @@
 import React, {useContext, useEffect, useState} from 'react';
-import apicoin from '../api/apicoin';
+import './userpage.css';
 import {Favorite} from '../userfav/Favorite';
 import axios from 'axios';
+import Coin from '../coin/Coin';
+
 
 
 
 const UserPage = () => {
     const [coins, setCoins] = useState([]);
     const {watchList} = useContext(Favorite)
+    const [isLoading, setIsLoading] = useState(false);
     console.log(watchList);
     useEffect(() => {
         const fatchData = async () => {
+            setIsLoading(true)
             const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
                 params: {
                     vs_currency: "usd",
-                    ids: "bitcoin",
+                    ids: watchList.join(","),
                 },
-
             });
-            console.log(response.data);
+            setCoins(response.data);
+            setIsLoading(false);
         }
         fatchData();
     },[]);
 
+    const renderCoin = () => {
+        if(isLoading) {
+            return <div>Loading...</div>
+        }
+        return (
+            <ul className="coinlist list-group mt-2">
+                {coins.map(coin => {
+                    return <Coin  
+                    key={coin.id} 
+                    name={coin.name} 
+                    image={coin.image} 
+                    symbol={coin.symbol} 
+                    volume={coin.total_volume} 
+                    price={coin.current_price}
+                    priceChange={coin.price_change_percentage_24h} 
+                    marketcap={coin.market_cap} />
+                })}
+            </ul>
+        )
+    }
+
     return (
         <div>
-
+            {renderCoin()}
         </div>
     )
   
