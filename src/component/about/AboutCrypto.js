8814dfdbd11debbdb8fe89f0ecdc100ce40d25f1
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-07 21:04:51 +0300

    about

--- src/component/about/AboutCrypto.js
+++ src/component/about/AboutCrypto.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './about.css';
 
 const AboutCrypto  = () => {
 
     return(
-        <div>
-            
+        <div className="info-container">
+        <div className="info">
+            <h1 className="title">What is cryptocurrency</h1>
+            <p className="info-more">Cryptocurrency is a form of payment that can be exchanged online for goods and services.
+               Many companies have issued their own currencies, often called tokens, and these can be traded specifically for the good or service that the company provides. Think of them as you would arcade tokens or casino chips.
+               You’ll need to exchange real currency for the cryptocurrency to access the good or service.
+               Cryptocurrencies work using a technology called blockchain. 
+               Blockchain is a decentralized technology spread across many computers that manages and records transactions. Part of the appeal of this technology is its security.
+            </p>
+        </div>
         </div>
     )
 
