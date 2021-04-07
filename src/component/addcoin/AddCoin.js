21e1eb946f40344552db5fae86d148218753e687
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-07 23:58:28 +0300

    add coin option

--- src/component/addcoin/AddCoin.js
+++ src/component/addcoin/AddCoin.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
 const AddCoin = () => {
-    
+    const optionalCoins = [
+        "bitcoin",
+        "ethereum",
+        "ripple",
+        "litecoin",
+        "cardano",
+        "the-graph",
+        "holotoken",
+        "polkadot",
+        "binancecoin",
+        "uniswap",
+        "chainlink",
+    ];
     
     return (
-        <div>
-            check
+        <div className="dropdown">
+            <button className="btn btn-primary dropdown-toggle" type="button">Add Coin</button>
+            <div className="dropdown-menu">
+                {optionalCoins.map((e) => {
+                    return (
+                        <a href="#" className="dropdown-item">
+                            {e}
+                        </a>
+                    )
+                })}
+
+            </div>
         </div>
     )
 
--- src/component/userpage/UserPage.js
+++ src/component/userpage/UserPage.js
@@ -3,6 +3,7 @@ import './userpage.css';
 import {Favorite} from '../userfav/Favorite';
 import axios from 'axios';
 import UserCoin from '../userCoin/UserCoin';
+import AddCoin from '../addcoin/AddCoin';
 
 
 
@@ -37,7 +38,9 @@ const UserPage = () => {
             return <div>Loading...</div>
         }
         return (
-           
+           <div>
+               <h1 style={{color:"#45a29e"}}> My List</h1>
+               <AddCoin />
             <ul className="coinlist list-group mt-2 shadow border p-2 rounded mt-2 container">
                 {coins.map(coin => {
                     return <UserCoin  
@@ -48,7 +51,7 @@ const UserPage = () => {
                 
                 
             </ul>
-           
+            </div>
         )
     }
 
