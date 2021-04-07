fb7aa4c031e64cbf4094aa99ee3124117feb64cb
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-07 18:55:39 +0300

    ..

--- src/component/coinchart/CoinChart.js
+++ src/component/coinchart/CoinChart.js
@@ -1,29 +1,13 @@
 import React, { useRef, useEffect } from 'react';
-import {Bar} from "react-chartjs-2";
-// import Chartjs from "chart.js";
 
 
 
-export const CoinChart = () => {
+
+const CoinChart = () => {
 
     return(
         <div>
-           <Bar 
-           data={{ 
-              labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [
-                {
-                    label: '# of vote',
-                    data: [12, 19, 3, 5, 2, 3]
-                }
-            ]
-            }}
-              height={400}
-              width={600}
-              options={{
-                  maintainAspectRatio: false,
-              }} 
-              />          
+                 
         </div>
     )
 }
