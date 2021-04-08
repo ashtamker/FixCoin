6077dab322dfd0c9c36e93ebf172702d382e91a7
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-08 10:52:21 +0300

    convert coded

--- src/component/convert/Convert.js
+++ src/component/convert/Convert.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CurrencyRow from '../currencyRow/CurrencyRow';
 
+// const BASE_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Clitecoin%2Cbinancecoin%2Cuniswap%2Cchainlink%2Cqtum&vs_currencies=usd';
+
 const Convert = () => {
+    
+
+//     const [currencyOptions, setCurrencyOptions] = useState([]);
+//     const [fromCurrency, setFromCurrency] = useState();
+//     const [toCurrency, setToCurrency] = useState();
+//     const [exchangeRate, setExchangeRate] = useState();
+//     const [amount, setAmount] = useState(1);
+//     const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+    
+//     let toAmount, fromAmount
+//   if (amountInFromCurrency) {
+//     fromAmount = amount
+//     toAmount = amount * exchangeRate
+//   } else {
+//     toAmount = amount
+//     fromAmount = amount / exchangeRate
+//   }
+
+//   useEffect(() => {
+//     fetch(BASE_URL)
+//       .then(data => {
+//         const firstCurrency = Object.keys(data.usd)[0]
+//         setCurrencyOptions([data.usd, ...Object.keys(data.usd)])
+//         setFromCurrency(data.usd)
+//         setToCurrency(firstCurrency)
+//         setExchangeRate(data.rates[firstCurrency])
+//       })
+//   }, [])
+
+//   useEffect(() => {
+//     if (fromCurrency != null && toCurrency != null) {
+//       fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+//         .then(data => setExchangeRate(data.usd[toCurrency]))
+//     }
+//   }, [fromCurrency, toCurrency])
+
+//   function handleFromAmountChange(e) {
+//     setAmount(e.target.value)
+//     setAmountInFromCurrency(true)
+//   }
+
+//   function handleToAmountChange(e) {
+//     setAmount(e.target.value)
+//     setAmountInFromCurrency(false)
+//   }
+
+
     return(
         <div>
             <h1 className="convert-title">Convert Currency</h1>
@@ -9,6 +58,24 @@ const Convert = () => {
             <div className="eq-currencyrow">=</div>
             <CurrencyRow />
         </div>
+        // <div>
+        //     <h1 className="convert-title">Convert Currency</h1>
+        //     <CurrencyRow
+        //       currencyOptions={currencyOptions}
+        //       selectedCurrency={fromCurrency}
+        //       onChangeCurrency={e => setFromCurrency(e.target.value)}
+        //       onChangeAmount={handleFromAmountChange}
+        //       amount={fromAmount}
+        //     />
+        //     <div className="eq-currencyrow">=</div>
+        //     <CurrencyRow 
+        //     currencyOptions={currencyOptions}
+        //     selectedCurrency={toCurrency}
+        //     onChangeCurrency={e => setToCurrency(e.target.value)}
+        //     onChangeAmount={handleToAmountChange}
+        //     amount={toAmount}
+        //     />
+        // </div>
     )
 
 }
--- src/component/currencyRow/CurrencyRow.js
+++ src/component/currencyRow/CurrencyRow.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import './currencyrow.css';
 
-const CurrencyRow = () => {
-    return(
+
+export default function CurrencyRow() {
+    return (
         <div className="main-currencyrow">
             <input type="number" className="input-currencyrow"></input>
             <select className="select-currencyrow">
-                <option value="Btc">Btc</option>
+                <option className="select-currencyrow" value="Btc">Btc</option>
+                <option className="select-currencyrow" value="Eth">Eth</option>
+                <option className="select-currencyrow" value="Bnb">Bnb</option>
+                <option className="select-currencyrow" value="Xrp">Xrp</option>
+                <option className="select-currencyrow" value="Ada">Ada</option>
             </select>
-            
         </div>
     )
-
 }
 
-export default CurrencyRow; 
+
+
+
+
+// export default function CurrencyRow(props)  {
+//     const {
+//         currencyOptions,
+//         selectedCurrency,
+//         onChangeCurrency,
+//         onChangeAmount,
+//         amount,
+//     } = props
+
+//     return(
+//         <div className="main-currencyrow">
+//             <input type="number" className="input-currencyrow" value={amount} onChange={onChangeAmount}></input>
+//             <select value={selectedCurrency} onChange={onChangeCurrency} className="select-currencyrow">
+//                 {currencyOptions.map(option => (
+//                     <option key={option} value={option}>{option}</option>
+//                 ))}
+                
+//             </select>
+            
+//         </div>
+//     )
+
+// }
+
